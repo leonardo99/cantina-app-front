d39dff1bde37c407d403a619e36006023e109103
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-22 14:54:50 -0300

    Retorna dados do usuario logado

---
+++ src/contexts/Auth/AuthContext.tsx
@@ -0,0 +1,36 @@
+
+import api from '@/services/api';
+import { createContext, useContext, useEffect, useState } from 'react';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface AuthContextType {
+  user: User | null;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null);
+
+    const fetchUser = async () => {
+        try {
+            const response = await api.get('/auth/user');
+            setUser(response.data);
+        } catch {
+            setUser(null);
+        }
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+
+}
