ba240a8105985a617f90c5d164ee0e9189fc4cd3
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-24 16:57:08 -0300

    Rotas privadas do cliente

--- src/Pages/Auth/PrivateRoutes.tsx
+++ src/Pages/Auth/PrivateRoutes.tsx
@@ -8,7 +8,7 @@ export default function PrivateRoute({ children }: { children: JSX.Element }) {
     const { user, loading } = useAuth();
     if (loading) return <div>Carregando...</div>;
            
-    if(user === null) return <Navigate to="/" />
+    if(user === null || user.type === "admin") return <Navigate to="/" />
 
     return (
         <>
