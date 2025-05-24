a80ed1c2da1b27f1c650c1ea5215f8135c4a9661
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-24 16:33:35 -0300

    Sidebar da area do cliente

--- src/Pages/Client/Home.tsx
+++ src/Pages/Client/Home.tsx
@@ -1,25 +1,37 @@
 
-
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import {  
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
+import Logo from "@/assets/logo.png";
+import Cart from "@/components/Client/cart/Cart";
 
-export default function Login() {
-    
+export default function Home() {
     return (
         <>
-            <div className="w-full h-lvh flex justify-center items-center">
-                <Card className="w-[350px]">
-                    <CardHeader className="text-center text-3xl">
-                        {/* <CardTitle className="flex flex-col gap-1 justify-center items-center">
-                            <img className="w-20 h-20" src={Logo} alt="logomarca" />
-                            CantinaApp
-                            </CardTitle>
-                        <CardDescription>Entre para iniciar sua sessão</CardDescription> */}
-                    </CardHeader>
-                    <CardContent>
-                        dashboard cliente
-                    </CardContent>
-                </Card>
+            <div className="flex items-center justify-between text-sm w-full p-3 bg-gray-100/20 shadow-b shadow-sm inset-shadow-gray-200">
+                <Link className="flex gap-2 justify-center items-center" to="/dashboard">
+                    <img className="w-10 h-10" src={Logo} alt="Logo"/>
+                    <h2 className="font-bold">CantinaApp</h2>
+                </Link>
+                <div className="flex justify-center items-center gap-1">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger 
+                            className={`flex items-center justify-center gap-2 text-gray-500 font-semibold cursor-pointer outline-none p-2`}>
+                                <FontAwesomeIcon icon={faUser} />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="mr-2">
+                            <DropdownMenuItem>Meus Pedidos</DropdownMenuItem>
+                            <DropdownMenuItem>Sair</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <Cart />
+                </div>
             </div>
         </>
     )
