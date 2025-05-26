36e88f8e84fd570d783188cbb0a40353b93ac9eb
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-26 00:01:18 -0300

    Layout para area de admin

--- src/Pages/Auth/PrivateRouteAdmin.tsx
+++ src/Pages/Auth/PrivateRouteAdmin.tsx
@@ -3,17 +3,50 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/contexts/Auth/AuthContext";
 import type { JSX } from "react";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuItem, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
+import SidebarItem from "@/components/Sidebar/SidebarItem";
 
-export default function PrivateRoute({ children }: { children: JSX.Element }) {
+export default function PrivateRouteAdmin({ children }: { children: JSX.Element }) {
     const { user, loading } = useAuth();
+    
     if (loading) return <div>Carregando...</div>;
-           
-    if(user === null || user.type === "student" || user.type === "responsible") return <Navigate to="/" />
+    
+    if(user === null || user.data.type != "admin") return <Navigate to="/" />
 
     return (
         <>
             <div className="w-full h-lvh">
-                { children }
+                 <SidebarProvider>
+                    <main>
+                        <SidebarTrigger />
+                        <Sidebar>
+                            <SidebarContent>
+                                <SidebarGroup>
+                                    <SidebarGroupLabel>CantinaApp</SidebarGroupLabel>
+                                    <SidebarGroupContent>
+                                        <SidebarMenu>
+                                        {/* {projects.map((project) => ( */}
+                                            <SidebarMenuItem 
+                                            // key={project.name}
+                                            >
+                                                <SidebarItem to="/admin/product/create">
+                                                    <FontAwesomeIcon icon={faBagShopping} />
+                                                    <span>Produtos</span>
+                                                </SidebarItem>
+                                            </SidebarMenuItem>
+                                        {/* ))} */}
+                                        </SidebarMenu>
+                                </SidebarGroupContent>
+                                </SidebarGroup>
+                            </SidebarContent>
+                        </Sidebar>
+                    </main>
+                    <div className="p-5 w-full">
+                        {children}
+                    </div>
+                </SidebarProvider>
             </div>
         </>
     )
