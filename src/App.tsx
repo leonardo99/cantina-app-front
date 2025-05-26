3d434ec7577d929ecd2c3d0cad87358cc07176cf
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-26 00:00:51 -0300

    rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { AuthProvider } from '@/contexts/Auth/AuthContext';
 import Login from '@/Pages/Auth/Login';
 import Home from '@/Pages/Client/Home';
 import PrivateRoute from '@/Pages/Auth/PrivateRoutes';
+import PrivateRouteAdmin from '@/Pages/Auth/PrivateRouteAdmin';
 import PublicRoute from '@/Pages/Auth/PublicRoutes';
+import Form from '@/Pages/Admin/Product/Form';
+import Index from '@/Pages/Admin/Product/Index';
 
 function App() {
   
@@ -18,6 +21,18 @@ function App() {
             <Route 
                    path='/dashboard' 
                    element={ <PrivateRoute><Home /></PrivateRoute> }
+            />
+            <Route 
+                   path='/dashboard' 
+                   element={ <PrivateRoute><Home /></PrivateRoute> }
+            />
+            <Route 
+                   path='/admin/product/' 
+                   element={ <PrivateRouteAdmin><Index /></PrivateRouteAdmin> }
+            />
+            <Route 
+                   path='/admin/product/create' 
+                   element={ <PrivateRouteAdmin><Form /></PrivateRouteAdmin> }
             />
           </Routes>
         </BrowserRouter>
