34a163f9efc4ee2a08c5580497ca315ef5eee0cd
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-27 14:07:12 -0300

    Listagem de itens

--- src/Pages/Client/Home.tsx
+++ src/Pages/Client/Home.tsx
@@ -1,50 +1,102 @@
 
-import {  
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { Link, useNavigate } from "react-router-dom";
-import Logo from "@/assets/logo.png";
-import Cart from "@/components/Client/cart/Cart";
-import { useAuth } from "@/contexts/Auth/AuthContext";
+import ProductItem from "@/components/Client/Item/Item";
+import SideBar from "@/components/Client/Sidebar/SideBar";
+import { FormControl } from "@/components/ui/form";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { getCategories } from "@/services/category/category";
+import { getProducts, getProductsByCategory, getProductsUser } from "@/services/product/product";
+import { useEffect, useState } from "react";
+
+interface Product {
+    id:number;
+    category_id: number;
+    category: string,
+    name: string;
+    amount: number;
+}
+interface Category {
+    id: number;
+    name: string;
+}
+
+interface ApiResponse<T> {
+    data: T;
+    links: any;
+    meta: any;
+}
 
 export default function Home() {
 
-    const { logout } = useAuth();
-    const navigate = useNavigate();
+    const [categories, setCategories] = useState<Category[] | undefined>(undefined);
+    const [products, setProducts] = useState<Product[] | undefined>(undefined);
+    const [selectedCategory, setSelectedCategory] = useState<string>("");
 
-    const handleLogout = async () => {
+    const fetchProducts = async () => {
         try {
-            await logout();
-            navigate("/");
-        } catch {
-            console.log("error");
+            const productsList = await getProductsUser();
+            setProducts(productsList?.data);
+        } catch (error) {
+            console.log(error);
         }
-    };
+    }
+
+    const fetchCategories = async () => {
+        try {
+            const categoriesList = await getCategories();
+            setCategories(categoriesList?.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleSelectCategoryChange = async (category_id: string) => {
+        try {
+            let response;
+            if(category_id=="0") {
+                response = await getProductsUser();
+                setProducts(response?.data);
+            }
+            else{
+                response = getProductsByCategory(category_id);
+                response.then((productsList) => {
+                    setProducts(productsList?.data);
+                });
+            }
+            
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCategories();
+    }, [])
 
     return (
         <>
-            <div className="flex items-center justify-between text-sm w-full p-3 bg-gray-100/20 shadow-b shadow-sm inset-shadow-gray-200">
-                <Link className="flex gap-2 justify-center items-center" to="/dashboard">
-                    <img className="w-10 h-10" src={Logo} alt="Logo"/>
-                    <h2 className="font-bold">CantinaApp</h2>
-                </Link>
-                <div className="flex justify-center items-center gap-1">
-                    <DropdownMenu>
-                        <DropdownMenuTrigger 
-                            className={`flex items-center justify-center gap-2 text-gray-500 font-semibold cursor-pointer outline-none p-2`}>
-                                <FontAwesomeIcon icon={faUser} />
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="mr-2">
-                            <DropdownMenuItem>Meus Pedidos</DropdownMenuItem>
-                            <DropdownMenuItem onClick={ () => handleLogout() }>Sair</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                    <Cart />
+            <SideBar/>
+            <div className="w-2/3 m-auto grid grid-cols-1 gap-3 gap-y-5 py-3">
+                <div className="lg:w-1/3 md:w-full flex items-center border-1 border-gray-200 px-1">
+                    <span className="flex text-xs font-medium justify-end">Filtrar por</span>
+                    <Select onValueChange={handleSelectCategoryChange}>
+                            <SelectTrigger className="w-full border-0 font-medium text-xs focus-visible:ring-0 flex-1">
+                                <SelectValue className="px-0" placeholder="Selecione uma categoria" />
+                            </SelectTrigger>
+                            <SelectContent className="flex grow">
+                                <SelectItem className="text-xs font-medium" aria-selected={true} value="0">Todos</SelectItem>
+                                {
+                                    categories?.map((item) => <SelectItem className="text-xs font-medium" aria-selected={true} key={item.id} value={String(item.id)}>{item.name}</SelectItem>)
+                                }
+                            </SelectContent>
+                    </Select>
+                </div>
+                <div className={`grid grid-cols-1 gap-3 gap-y-5 ${products?.length ? "md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-3" : ""}`}>
+                    {
+                        products?.length ? (products?.map((item) => ( 
+                            <ProductItem key={item.id} data={item}/>
+                        ))) : <div className="w-full">Nenhum item encontrado</div>
+                    }
                 </div>
             </div>
         </>
