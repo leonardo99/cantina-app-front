4929e605b1ebf528940e5fa532c25c3e8eceea2f
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-28 20:56:26 -0300

    Insere item ao carrinho

--- src/components/Client/Item/Item.tsx
+++ src/components/Client/Item/Item.tsx
@@ -1,12 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader } from "@/components/ui/card";
+import { CartProvider } from "@/contexts/Cart/CartContext";
+import { useCart } from "@/contexts/Cart/CartContext";
 
 interface Product {
     id:number;
     category_id: number;
+    cart_item_id: number;
+    cart_id: number;
     category: string,
     name: string;
     amount: number;
+    quantity: number
+    brute_amount: number;
 }
 
 interface Item {
@@ -15,17 +21,21 @@ interface Item {
 
 export default function ProductItem({ data }: Item) {
 
+    const { addToCart } = useCart();
+
     return (
         <>
-            <Card className="rounded-sm pb-0">
-                <CardContent>
-                   <h2 className="text-md">{data.name}</h2>
-                   <h3 className="text-sm font-medium">{data.amount}</h3>
-                </CardContent>
-                <CardFooter className="p-0 sm:p-2.5">
-                    <Button className="w-full rounded-none rounded-bl-md rounded-br-md cursor-pointer md:rounded-md md:text-xs">Adicionar ao carrinho</Button>
-                </CardFooter>
-            </Card>
+            <CartProvider>
+                <Card className="rounded-sm pb-0">
+                    <CardContent>
+                    <h2 className="text-md">{data.name}</h2>
+                    <h3 className="text-sm font-medium">{data.amount}</h3>
+                    </CardContent>
+                    <CardFooter className="p-0 sm:p-2.5">
+                        <Button onClick={() => addToCart(data) } className="w-full rounded-none rounded-bl-md rounded-br-md cursor-pointer md:rounded-md md:text-xs">Adicionar ao carrinho</Button>
+                    </CardFooter>
+                </Card>
+            </CartProvider>
         </>
     )
 };
