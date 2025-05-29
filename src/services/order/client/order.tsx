9a3858687a1dc85f4a2ea7f8afd9f8991b00c3b6
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-29 14:55:43 -0300

    Seta status do pedido e lista pedidos por status

--- src/services/order/client/order.tsx
+++ src/services/order/client/order.tsx
@@ -1,11 +1,22 @@
 import api from "@/services/api";
+interface Product {
+    name: string
+}
+interface OrderItem {
+    id: number,
+    order_id: number,
+    amount: number,
+    price: number
+    items: Product
+}
 interface Order {
     id: number,
     user_id: number,
     responsible_id: number,
     dependent_id: number,
     total_value: number,
-    status: string
+    status: string,
+    items: OrderItem[],
 }
 
 interface ApiResponse<T> {
@@ -17,6 +28,28 @@ interface ApiResponse<T> {
 export const getOrders = async (): Promise<ApiResponse<Order[]> | undefined> => {
     try {
         const response = await api.get<ApiResponse<Order[]>>("/user/order");
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return undefined;
+    }
+};
+
+export const updateStatusOrder = async (id: number, status: object): Promise<ApiResponse<Order> | undefined> => {
+    try {
+        const response = await api.patch<ApiResponse<Order>>(`/admin/order/${id}`, status);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return undefined;
+    }
+};
+
+export const getOrdersByStatus = async (data: object): Promise<ApiResponse<Order[]> | undefined> => {
+    try {
+        const response = await api.get<ApiResponse<Order[]>>("/admin/order",  {
+            params: data
+        });
         return response.data;
     } catch (error) {
         console.error(error);
