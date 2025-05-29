df57e9c56f470102857d75623a03b202144095cd
Leonardo Lima <llimaesilva7@gmail.com>
2025-05-29 07:57:20 -0300

    Verifica se o usuario tem permissão para acessar a rota

--- src/Pages/Auth/PrivateRoutes.tsx
+++ src/Pages/Auth/PrivateRoutes.tsx
@@ -8,7 +8,7 @@ export default function PrivateRoute({ children }: { children: JSX.Element }) {
     const { user, loading } = useAuth();
     if (loading) return <div>Carregando...</div>;
            
-    if(user === null || user.type === "admin") return <Navigate to="/" />
+    if(user === null || user.data.type === "admin") return <Navigate to="/" />
 
     return (
         <>
